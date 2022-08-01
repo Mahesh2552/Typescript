7c9d4064e6cce5d5b0f429094b73914c8d92fcbc
Mahesh Ramdas Dubal <104274059+Mahesh2552@users.noreply.github.com>
2022-08-01 15:33:55 +0530

    Update 14_RestParam.ts

--- 14_RestParam.ts
+++ 14_RestParam.ts
@@ -1,8 +1,4 @@
-/*
-Variable number of arguments /Rest Parameters
-Rest parameters don’t restrict the number of values that you can pass to a function. However, the values passed must all be of the same type.
-To declare a rest parameter, the parameter name is prefixed with three periods. Any nonrest parameter should come before the rest parameter.
-*/
+
  
 function fun(...nums:number[]) 
 {  
